7f2f343a729cccd6f96731311d1e29dae7b1acf8
crablet <love4uandgzfc@gmail.com>
2020-03-25 21:01:28 +0800

    实现无边框置顶背景穿透可拖动

--- MianPetClient/Src/PetClient.cpp
+++ MianPetClient/Src/PetClient.cpp
@@ -1,11 +1,48 @@
 ﻿#include "PetClient.h"
 
 PetClient::PetClient(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), isUiBeingDragging(false)
 {
+    petGif = new QMovie(R"(./Pic/test.gif)", {}, this);
+    petGifLabel = new QLabel(this);
+    petGifLabel->move(0, 0);
+    petGifLabel->setMovie(petGif);
+    petGif->start();
+
+    setWindowFlags(windowFlags() | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    setAttribute(Qt::WA_TranslucentBackground);
+    setFixedSize(petGif->currentImage().size());
 }
 
-PetClient::~PetClient()
+void PetClient::mouseMoveEvent(QMouseEvent *event)
 {
+    if (event->buttons() & Qt::LeftButton)
+    {
+        if (isUiBeingDragging)
+        {
+            const auto delta = event->globalPos() - previousMousePos;
+            this->move(previousUiPos + delta);
+        }
+    }
+
+    QWidget::mouseMoveEvent(event);
 }
 
+void PetClient::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        isUiBeingDragging = true;
+        previousMousePos = event->globalPos();
+        previousUiPos = frameGeometry().topLeft();
+    }
+
+    QWidget::mousePressEvent(event);
+}
+
+void PetClient::mouseReleaseEvent(QMouseEvent *event)
+{
+    isUiBeingDragging = false;
+
+    QWidget::mouseReleaseEvent(event);
+}
--- MianPetClient/Src/PetClient.h
+++ MianPetClient/Src/PetClient.h
@@ -2,6 +2,9 @@
 #define PETCLIENT_H
 
 #include <QWidget>
+#include <QMouseEvent>
+#include <QMovie>
+#include <QLabel>
 
 class PetClient : public QWidget
 {
@@ -9,6 +12,18 @@ class PetClient : public QWidget
 
 public:
     PetClient(QWidget *parent = nullptr);
-    ~PetClient();
+
+private:
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void mouseReleaseEvent(QMouseEvent *event) override;
+    //virtual void enterEvent(QEvent *event) override;
+    //virtual void leaveEvent(QEvent *event) override;
+
+private:
+    QMovie *petGif;
+    QLabel *petGifLabel;
+    QPoint previousMousePos, previousUiPos;
+    bool isUiBeingDragging;
 };
 #endif // PETCLIENT_H
