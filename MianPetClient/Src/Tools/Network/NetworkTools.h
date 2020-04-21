9c6fe74922613cda47163af5a8646cd1c719c86e
crablet <love4uandgzfc@gmail.com>
2020-04-21 22:28:29 +0800

    新增GetRandomKeyForPasswordTransportation函数组，用于生成加密传输中的RandomKey

--- MianPetClient/Src/Tools/Network/NetworkTools.h
+++ MianPetClient/Src/Tools/Network/NetworkTools.h
@@ -1,4 +1,23 @@
 #ifndef NETWORKTOOLS_H
 #define NETWORKTOOLS_H
 
+#include <random>
+#include <string>
+
+#include <QString>
+
+inline std::string GetRandomKeyForPasswordTransportation(const std::string &coreKey) noexcept
+{
+    std::random_device rd;
+    std::minstd_rand gen(rd());
+    std::uniform_int_distribution<> dis(100000, 999999);
+
+    return std::to_string(dis(gen)) + '@' + coreKey + '@' + std::to_string(dis(gen));
+}
+
+inline QString GetRandomKeyForPasswordTransportation(const QString &coreKey) noexcept
+{
+    return QString::fromStdString(GetRandomKeyForPasswordTransportation(coreKey.toStdString()));
+}
+
 #endif // NETWORKTOOLS_H
