445df43a41d8bb754de01ad6900df9e4a7f3e98a
crablet <love4uandgzfc@gmail.com>
2020-04-30 17:25:37 +0800

    调整项目结构

--- MianPetServer/Src/main.cpp
+++ MianPetServer/Src/main.cpp
@@ -1,8 +1,17 @@
 ﻿#include <iostream>
+#include <asio.hpp>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    asio::io_context io;
+
+    asio::steady_timer t(io, asio::chrono::seconds(5));
+    t.async_wait([](const asio::error_code &e)
+    {
+        std::cout << "Hello World!" << std::endl;
+    });
+
+    io.run();
 
     return 0;
 }
