95ca54a8aa9a582f2bcbaf50335404eceb229332
crablet <love4uandgzfc@gmail.com>
2020-05-17 23:16:18 +0800

    更新注释

--- MianPetClient/Src/Tools/Network/DataPackages/LogoutData.h
+++ MianPetClient/Src/Tools/Network/DataPackages/LogoutData.h
@@ -7,13 +7,21 @@
 
 #include "Src/Config/NetworkConstants.h"
 
+// 用于传递登出消息的json文本
+// 其"payload"字段拥有如下格式：
+// "payload":
+// {
+//   "random_key": string
+// }
 class LogoutData : public DataPackage
 {
 public:
     LogoutData();
 
 private:
+    // 设置"random_key"字段的值
     void SetRandomKey(const QString &randomKey);
+    // 获取"random_key"字段的值
     QString GetRandomKey() const;
 };
 
