790dd936f7b6890e61d55296cdd63deec625da5c
crablet <love4uandgzfc@gmail.com>
2020-05-23 23:51:52 +0800

    新增对CorekeyRequestData的注释

--- MianPetClient/Src/Tools/Network/DataPackages/CorekeyRequestData.h
+++ MianPetClient/Src/Tools/Network/DataPackages/CorekeyRequestData.h
@@ -3,6 +3,17 @@
 
 #include "RequestData.h"
 
+// 用于向服务器请求用于加密的corekey时的数据包
+// 其拥有以下格式：
+//{
+//  "version": int,
+//  "id": string,
+//  "method": GET,
+//  "hint": CORE_KEY_FOR_PASSWORD_TRANSPORTATION,
+//  "payload":
+//  {
+//  }
+//}
 class CorekeyRequestData : public RequestData
 {
 public:
