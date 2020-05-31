bc88793eb7c270cfbcaecd2634cc983cf5cc63f3
crablet <love4uandgzfc@gmail.com>
2020-05-31 20:37:19 +0800

    新增RequestData类的注释

--- MianPetClient/Src/Tools/Network/DataPackages/RequestData.h
+++ MianPetClient/Src/Tools/Network/DataPackages/RequestData.h
@@ -3,6 +3,16 @@
 
 #include "DataPackage.h"
 
+// 该类是所有用于请求数据的数据包的基类，一般不直接使用，而是通过继承使用
+// 其拥有以下格式：
+//{
+//  "version": int,
+//  "id": string,
+//  "method": GET,
+//  "payload":
+//  {
+//  }
+//}
 class RequestData : public DataPackage
 {
 public:
