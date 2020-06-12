f67ef78551b4e7b1187d3d8f0a163fc028444eea
crablet <love4uandgzfc@gmail.com>
2020-06-13 01:04:23 +0800

    更新注释

--- MianPetServer/Src/Connection.cpp
+++ MianPetServer/Src/Connection.cpp
@@ -164,7 +164,7 @@ void Connection::DealWithGetCoreKeyForPasswordTransportation(const char *id)
 
 void Connection::DealWithGetLogin(const char *id, const char *password, const char *randomKey)
 {
-    // TODO: 这一块似乎有bug，现在无法登出了。时而可以复现，时而不行，暂无100%复现的方法。
+    // TODO: 登入/登出时偶现corekey传递失败的问题
 
     try
     {
@@ -399,8 +399,6 @@ void Connection::DealWithHeartbeat(const char *id, const char *randomKey)
 
 void Connection::DealWithLogout(const char *id, const char *randomKey)
 {
-    // TODO: 这一块似乎有bug，现在无法登出了。时而可以复现，时而不行，暂无100%复现的方法。
-
     try
     {
         std::lock_guard<std::mutex> lock(dbMutex);
