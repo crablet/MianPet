4e2c7aeb40223c3d3a03e04d4f52c136ec9b6523
crablet <love4uandgzfc@gmail.com>
2020-11-07 23:35:38 +0800

    更新注释

--- MianPetClient/Src/PetToolButtonsContainer.h
+++ MianPetClient/Src/PetToolButtonsContainer.h
@@ -24,17 +24,17 @@ private:
     void InitializeConnect();
 
 private slots:
-    void OnFoodButtonClicked();     // 当点击喂食按钮时会发出此信号
-    void OnCleanButtonClicked();    // 当点击清洁按钮时会发出此信号
-    void OnWorkingButtonClicked();  // 当点击打工按钮时会发出此信号
+    void OnFoodButtonClicked();         // 当点击喂食按钮时会发出此信号
+    void OnCleanButtonClicked();        // 当点击清洁按钮时会发出此信号
+    void OnWorkingButtonClicked();      // 当点击打工按钮时会发出此信号
     void OnPetProfileButtonClicked();   // 当点击面宠属性菜单时会发出此信号
 
 private:
-    QHBoxLayout *layout;    // 主体布局，呈一字型排开
+    QHBoxLayout *layout;                // 主体布局，呈一字型排开
     QPushButton *foodButton, *cleanButton, *petProfileButton, *workingButton;
-    FoodWindow *foodWindow;         // 食物窗口，为人为保证的单例
-    CleanWindow *cleanWindow;       // 洗澡窗口，为人为保证的单例
-    WorkingWindow *workingWindow;   // 打工窗口，为人为保证的单例
+    FoodWindow *foodWindow;             // 食物窗口，为人为保证的单例
+    CleanWindow *cleanWindow;           // 洗澡窗口，为人为保证的单例
+    WorkingWindow *workingWindow;       // 打工窗口，为人为保证的单例
     PetProfileWindow *petProfileWindow; // 面宠属性窗口，为人为保证的单例
 };
 
