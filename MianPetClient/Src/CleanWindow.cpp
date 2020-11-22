a1bd5e1f2507b20dfaa798a296a503657f5849a7
crablet <love4uandgzfc@gmail.com>
2020-11-22 23:12:51 +0800

    更新注释

--- MianPetClient/Src/CleanWindow.cpp
+++ MianPetClient/Src/CleanWindow.cpp
@@ -135,7 +135,7 @@ void CleanWindow::InitializeConnect()
     connect(item3, &QPushButton::clicked, this, [=]()
     {
         if (const auto index = currentPage * 4 + 3;
-            index < items.size())
+            index < items.size())   // 确保index不要越界，因为有可能有按钮没有对应的清洁品
         {
             selectedClean = items[index].name;
 
