f11b00c434a69318b8fee044e83858a613fd0c1d
crablet <love4uandgzfc@gmail.com>
2020-12-05 20:25:27 +0800

    更新注释

--- MianPetClient/Src/FoodWindow.cpp
+++ MianPetClient/Src/FoodWindow.cpp
@@ -199,7 +199,7 @@ void FoodWindow::DataPrepare()
         : item0->setIcon(QIcon());  // 如果越界了就展示空图标
     rangeBegin + 1 < rangeEnd
         ? item1->setIcon(QIcon(":/Pic/" + items[rangeBegin + 1].name + ".png"))
-        : item1->setIcon(QIcon());
+        : item1->setIcon(QIcon());  // 如果越界了就展示空图标
     rangeBegin + 2 < rangeEnd
         ? item2->setIcon(QIcon(":/Pic/" + items[rangeBegin + 2].name + ".png"))
         : item2->setIcon(QIcon());
