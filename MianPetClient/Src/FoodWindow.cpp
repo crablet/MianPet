51888fd80cb82c3b7b141b53cd430e489f78997b
crablet <love4uandgzfc@gmail.com>
2020-12-07 20:14:25 +0800

    更新注释

--- MianPetClient/Src/FoodWindow.cpp
+++ MianPetClient/Src/FoodWindow.cpp
@@ -223,11 +223,11 @@ void FoodWindow::ViewPreviousPage()
         std::thread requestThread(&FoodWindow::RequestDataInRange, this, rangeBegin, rangeEnd);
         requestThread.detach();
 
+        // 展示[rangeBegin, rangeEnd)中的内容
         item0->setIcon(QIcon(":/Pic/" + items[rangeBegin + 0].name + ".png"));
         item1->setIcon(QIcon(":/Pic/" + items[rangeBegin + 1].name + ".png"));
         item2->setIcon(QIcon(":/Pic/" + items[rangeBegin + 2].name + ".png"));
         item3->setIcon(QIcon(":/Pic/" + items[rangeBegin + 3].name + ".png"));
-        // 展示[rangeBegin, rangeEnd)中的内容
 
         selectedFood.clear();
         buyButton->setDisabled(true);
