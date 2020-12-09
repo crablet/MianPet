3054fd2b68a904b902ae41ed11dd3e00e7f8af52
crablet <love4uandgzfc@gmail.com>
2020-12-09 17:34:28 +0800

    更新注释

--- MianPetClient/Src/CleanWindow.cpp
+++ MianPetClient/Src/CleanWindow.cpp
@@ -247,7 +247,7 @@ void CleanWindow::ViewNextPage()
         requestThread.detach();
 
         // 展示[rangeBegin, rangeEnd)中的内容
-        rangeBegin + 0 < rangeEnd
+        rangeBegin + 0 < rangeEnd   // 确保不要越界
             ? item0->setIcon(QIcon(":/Pic/" + items[rangeBegin + 0].name + ".png"))
             : item0->setIcon(QIcon());
         rangeBegin + 1 < rangeEnd
