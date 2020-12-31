43495510eda4b9051143bacc041e66a0bd29eafd
crablet <love4uandgzfc@gmail.com>
2020-12-31 08:29:04 +0800

    更新注释

--- MianPetClient/Src/CleanWindow.cpp
+++ MianPetClient/Src/CleanWindow.cpp
@@ -194,7 +194,7 @@ void CleanWindow::DataPrepare()
     const auto rangeEnd = static_cast<decltype(rangeBegin)>(items.size() >= 4 ? 4 : items.size());
     rangeBegin + 0 < rangeEnd   // 确保不要越界
         ? item0->setIcon(QIcon(":/Pic/" + items[rangeBegin + 0].name + ".png"))
-        : item0->setIcon(QIcon());
+        : item0->setIcon(QIcon());  // 如果越界了就展示空图标
     rangeBegin + 1 < rangeEnd   // 确保不要越界
         ? item1->setIcon(QIcon(":/Pic/" + items[rangeBegin + 1].name + ".png"))
         : item1->setIcon(QIcon());
