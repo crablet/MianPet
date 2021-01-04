67654fab4921d321b5f6027656965fe1ff0ff11a
crablet <love4uandgzfc@gmail.com>
2021-01-04 23:39:28 +0800

    更新注释

--- MianPetClient/Src/CleanWindow.cpp
+++ MianPetClient/Src/CleanWindow.cpp
@@ -82,7 +82,7 @@ void CleanWindow::InitializeConnect()
             buyButton->setEnabled(true);
             useButton->setEnabled(true);
         }
-        else
+        else    // 越界处理
         {
             selectedClean.clear();
 
