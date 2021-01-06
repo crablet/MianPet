e58ddfe7853bef41a3491264950b133687d3ba6f
crablet <love4uandgzfc@gmail.com>
2021-01-06 23:41:08 +0800

    更新注释

--- MianPetClient/Src/FoodWindow.cpp
+++ MianPetClient/Src/FoodWindow.cpp
@@ -144,7 +144,7 @@ void FoodWindow::InitializeConnect()
             buyButton->setEnabled(true);
             useButton->setEnabled(true);
         }
-        else
+        else    // 越界处理
         {
             selectedFood.clear();
 
