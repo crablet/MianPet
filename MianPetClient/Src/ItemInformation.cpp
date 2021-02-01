4d17801c6a55895adcc1a977dfe296bad0eb83b0
crablet <love4uandgzfc@gmail.com>
2021-02-01 22:48:06 +0800

    为ItemInformation类添加了Getter和Setter

--- MianPetClient/Src/ItemInformation.cpp
+++ MianPetClient/Src/ItemInformation.cpp
@@ -3,3 +3,83 @@
 //
 
 #include "ItemInformation.h"
+
+const QString &ItemInformation::GetName() const
+{
+    return name;
+}
+
+void ItemInformation::SetName(const QString &name)
+{
+    this->name = name;
+}
+
+int ItemInformation::GetPrice() const
+{
+    return price;
+}
+
+void ItemInformation::SetPrice(int price)
+{
+    this->price = price;
+}
+
+int ItemInformation::GetAmount() const
+{
+    return amount;
+}
+
+void ItemInformation::SetAmount(int amount)
+{
+    this->amount = amount;
+}
+
+const QString &ItemInformation::GetDescription() const
+{
+    return description;
+}
+
+void ItemInformation::SetDescription(const QString &description)
+{
+    this->description = description;
+}
+
+int ItemInformation::GetType() const
+{
+    return type;
+}
+
+void ItemInformation::SetType(int type)
+{
+    this->type = type;
+}
+
+int ItemInformation::GetFood() const
+{
+    return food;
+}
+
+void ItemInformation::SetFood(int food)
+{
+    this->food = food;
+}
+
+int ItemInformation::GetClean() const
+{
+    return clean;
+}
+
+void ItemInformation::SetClean(int clean)
+{
+    this->clean = clean;
+}
+
+int ItemInformation::GetHealth() const
+{
+    return health;
+}
+
+void ItemInformation::SetHealth(int health)
+{
+    this->health = health;
+}
--- MianPetClient/Src/ItemInformation.h
+++ MianPetClient/Src/ItemInformation.h
@@ -9,14 +9,43 @@
 #include <QString>
 #include <utility>
 
+// 用于表示商店能买的一般商品的数据
 class ItemInformation
 {
+public:
+    ItemInformation() = default;
     ItemInformation
     (QString name, int price, int amount, QString description, int type, int food, int clean, int health)
             : name(std::move(name)), price(price), amount(amount), description(std::move(description)), type(type), food(food), clean(clean), health(health)
     {
     }
 
+    [[nodiscard]] const QString &GetName() const;
+    void SetName(const QString &name);
+
+    [[nodiscard]] int GetPrice() const;
+    void SetPrice(int price);
+
+    [[nodiscard]] int GetAmount() const;
+    void SetAmount(int amount);
+
+    [[nodiscard]] const QString &GetDescription() const;
+    void SetDescription(const QString &description);
+
+    [[nodiscard]] int GetType() const;
+    void SetType(int type);
+
+    [[nodiscard]] int GetFood() const;
+    void SetFood(int food);
+
+    [[nodiscard]] int GetClean() const;
+    void SetClean(int clean);
+
+    [[nodiscard]] int GetHealth() const;
+    void SetHealth(int health);
+
+
+private:
     QString name;
     int price;
     int amount;
